eebaf4ef1c3f8c1ef0e893158a55cf514c42c9af
Zech Zimmerman <hi@zech.codes>
2020-07-12 18:30:36 -0400

    Add challenge test framework

--- beginnerpy/challenges/adventure_game/__main__.py
+++
@@ -1,7 +0,0 @@
-from beginnerpy.challenges.adventure_game.tests import run_tests
-import datetime
-import random
-
-
-random.seed(datetime.datetime.utcnow().replace(hour=0, minute=0, second=0, microsecond=0).timestamp())
-run_tests()
